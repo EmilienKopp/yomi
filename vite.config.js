6ff48156134e02ef21ca15b1a55b6d481229f755
EmilienKopp <emilien.kopp@gmail.com>
2025-03-05 01:19:25 +0900

    Add Svelte support and update project configuration

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,47 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue';
+import { URL, fileURLToPath } from "node:url";
+import { svelte, vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.ts',
-            ssr: 'resources/js/ssr.ts',
+            input: ["resources/js/app.js", "resources/svelte/app.js"],
             refresh: true,
         }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
+        svelte({
+            preprocess: vitePreprocess(),
+            compilerOptions: {
+                runes: true,
             },
         }),
     ],
+    resolve: {
+        alias: {
+            $lib: fileURLToPath(new URL("./resources/js/Lib", import.meta.url)),
+            $components: fileURLToPath(
+                new URL("./resources/js/Components", import.meta.url)
+            ),
+            $vendor: fileURLToPath(new URL("./vendor", import.meta.url)),
+            $types: fileURLToPath(
+                new URL("./resources/js/types", import.meta.url)
+            ),
+            $layouts: fileURLToPath(
+                new URL("./resources/js/Layouts", import.meta.url)
+            ),
+            $pages: fileURLToPath(
+                new URL("./resources/js/Pages", import.meta.url)
+            ),
+            $lang: fileURLToPath(new URL("./lang", import.meta.url)),
+            $models: fileURLToPath(
+                new URL("./resources/js/models.d.ts", import.meta.url)
+            ),
+        },
+    },
+    server: {
+        hmr: {
+            clientPort: 5173,
+        },
+    },
 });
